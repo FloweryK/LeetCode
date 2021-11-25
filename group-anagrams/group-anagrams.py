34b9118ae3a521bde9c0583afcf226b650eb9f0a
Minsang Yu <sunflower_94@kaist.ac.kr>
2021-11-26 00:31:11 +0900

    Time: 96 ms (81.92%), Space: 17.2 MB (78.53%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,12 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
+        anagrams = collections.defaultdict(list)
+        
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            
-            if sorted_word in anagrams:
-                anagrams[sorted_word].append(word)
-            else:
-                anagrams[sorted_word] = [word]
+            anagrams[''.join(sorted(word))].append(word)
         
         return list(anagrams.values())
+        
+#         # Naive solution
+#         anagrams = {}
+#         for word in strs:
+#             sorted_word = ''.join(sorted(word))
+            
+#             if sorted_word in anagrams:
+#                 anagrams[sorted_word].append(word)
+#             else:
+#                 anagrams[sorted_word] = [word]
+        
+#         return list(anagrams.values())
