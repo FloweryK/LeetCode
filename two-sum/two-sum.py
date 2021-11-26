f391d0fb87a92ad8fad2b5d981e454011fe31cbd
Minsang Yu <sunflower_94@kaist.ac.kr>
2021-11-26 17:16:39 +0900

    Time: 77 ms (52.61%), Space: 15.7 MB (9.01%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Naive solution
         nums = [(i, num) for i, num in enumerate(nums)]
         nums = sorted(nums, key=lambda x: x[1])
         
@@ -12,3 +13,11 @@ class Solution:
                 right -= 1
         
         return [nums[left][0], nums[right][0]]
+        
+#         # solution with hash
+#         hash = {num: i for i, num in enumerate(nums)}
+        
+#         for i, num in enumerate(nums):
+#             if (target-num in hash) and (i != hash[target-num]):
+#                 return [i, hash[target-num]]
+        
