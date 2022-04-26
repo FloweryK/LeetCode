cb9dd8103b990def460ea8e3424131d3bf70d4af
Minsang Yu <sunflower_94@kaist.ac.kr>
2022-04-27 00:58:27 +0900

    Time: 237 ms (95.49%), Space: 13.9 MB (92.61%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -10,7 +10,7 @@ class Solution:
             return s[left+1:right]
         
         result = ''
-        for i in range(len(s)):
+        for i in range(len(s)-1):
             result = max(result, expand(i, i+1), expand(i, i+2), key=len)
         
         return result
