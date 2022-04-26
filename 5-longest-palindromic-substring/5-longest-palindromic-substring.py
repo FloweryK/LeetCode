2ce0859ea399fbcdf0f359f63e81df3712d81ae8
Minsang Yu <sunflower_94@kaist.ac.kr>
2022-04-27 00:51:22 +0900

    Time: 241 ms (95.41%), Space: 14 MB (63.45%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -3,10 +3,15 @@ class Solution:
         if len(s) < 2 or s == s[::-1]:
             return s
         
-        for k in reversed(range(1, len(s)+1)):
-            for i in range(len(s)-(k-1)):
-                subset = s[i:i+k]
-                if subset == subset[::-1]:
-                    return subset
-                
-                
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        result = ''
+        for i in range(len(s)-1):
+            result = max(result, expand(i, i+1), expand(i, i+2), key=len)
+        
+        return result
+        
