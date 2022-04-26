d527f1652b294356c374b5834b281df1d9813997
Minsang Yu <sunflower_94@kaist.ac.kr>
2022-04-27 01:55:47 +0900

    Time: 499 ms (26.91%), Space: 34.5 MB (5.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,19 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        adj = defaultdict(list)
+        not_visited = set()
+        
         m = len(grid)
         n = len(grid[0])
-        
-        adj = defaultdict(list)
-        left = set()
-        visited = [True for _ in range(m*n)]
         
         for i in range(m):
             for j in range(n):
                 if int(grid[i][j]):
+                    not_visited.add(i*n+j)
                     adj[i*n+j].append(i*n+j)
-                    left.add(i*n+j)
-                    visited[i*n+j] = False
                     
                     if i-1 >= 0 and int(grid[i-1][j]):
                         adj[i*n+j].append((i-1)*n+j)
@@ -27,23 +25,20 @@ class Solution:
                         
                     if j+1 <= n-1 and int(grid[i][j+1]):
                         adj[i*n+j].append(i*n+(j+1))
-                        
+                   
         answer = 0
         stack = []
         
-        while left or stack:
+        while not_visited or stack:
             if not stack:
                 answer += 1
-                i = left.pop()
+                i = not_visited.pop()
             else:
                 i = stack.pop()
-            
-            visited[i] = True
                 
             for j in adj[i]:
-                if not visited[j]:
-                    visited[j] = True
-                    left.remove(j)
+                if j in not_visited:
+                    not_visited.remove(j)
                     stack.append(j)
         
         return answer
